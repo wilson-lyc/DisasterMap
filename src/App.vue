fbcd61cca7f080e0ff3435473ed804beca84adae
wilson <wilson_lyc@foxmail.com>
2025-11-18 15:59:37 +0800

    更新备案号

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,12 @@
     <el-menu-item index="/panel_b">Interactive Map</el-menu-item>
     <el-menu-item>
       <template #default>
-        <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023033182号-2</a>
+        <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023033182号</a>
       </template>
     </el-menu-item>
   </el-menu>
   <div id="main">
     <RouterView />
-  </div>
-  <div class="icp">
-
   </div>
 </template>
 
